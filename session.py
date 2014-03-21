c1aa8aa374c6689976897b463c0021ec2fe0e989
ccelest <ccelest@KGTui-MacBook-Pro.local>
2014-03-21 15:21:04 +0900

    kakao script deleted

--- session.py
+++
@@ -1,8 +0,0 @@
-from pykakao import kakaotalk
-
-kakao = kakaotalk()
-if kakao.auth("ccelest@kaist.ac.kr", "kt2714", "MBPR", "1"):
-    print kakao.session_key
-    print kakao.user_id
-else:
-    print "auth failed"
