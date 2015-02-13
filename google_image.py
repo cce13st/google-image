af57a728c9a6f569e186cf25ad062d5d426883f0
ccelest <cce13st@gmail.com>
2015-02-14 02:51:58 +0900

    Code merged into 1 file, and needless terms removed

--- google_image.py
+++ google_image.py
@@ -1,44 +1,71 @@
 # coding:utf-8
-
 import json
-import os
-import time
 import requests
-from PIL import Image
-from StringIO import StringIO
-from requests.exceptions import ConnectionError
 import random
 import urllib
-import types
+import os
 
-def image_search(query, path, num):
-    """Download full size images from Google image search.
-        
-        Don't print or republish images without permission.
-        I used this to train a learning algorithm.
-        """
-    BASE_URL = 'https://ajax.googleapis.com/ajax/services/search/images?'\
-        'v=1.0&q=' + query + '&start=%d'
-    
-    index = random.randint(0, 20)
-    r = requests.get(BASE_URL % 0)
+def downloadImage(url_list, directory):
+    '''
+    "image_search" finds number of images from google image search,
+    and store it to target directory
+
+    Parameters
+        url_list : list of string url
+        directory : directory for store images
+
+    Return value
+        return number of successfully downloaded images
+    '''
+
     cnt = 0
 
-    if len(json.loads(r.text)['responseData']['results']) == 0:
-        return False
+    if len(url_list) == 0:
+        print "url_list is empty!"
+        return 0
 
-    for image_info in json.loads(r.text)['responseData']['results']:
-        url = image_info['unescapedUrl']
-        if (cnt >= index):
-	    break
-	cnt += 1
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
-    if url == "":
-        return False
-    if not urllib.urlretrieve(url, "images/" + str(num) + ".jpg"):
-        return False
+    for url in url_list:
+        if url == "":
+            continue
+        if not urllib.urlretrieve(url, directory + "/" + str(cnt) + ".jpg"):
+            continue
+        cnt += 1
 
-    return True
+    return cnt
 
-# Example use
-#go('수지', 'myDirectory')
+def searchImageUrl(query, num):
+    '''
+    "searchImageUrl" finds number of images from google image search,
+    and return found images url
+
+    Parameters
+        query : query string for searching
+        num : # of images to find
+
+    Return value
+        return a tuple (# of images found, num)
+    '''
+
+    BASE_URL = 'https://ajax.googleapis.com/ajax/services/search/images?'\
+        'v=1.0&q=' + query + '&start=%d'
+   
+    url_list = []
+
+    for i in xrange(num) :
+        r = requests.get(BASE_URL % i)
+
+        if len(json.loads(r.text)['responseData']['results']) == 0:
+            return []
+
+        for image_info in json.loads(r.text)['responseData']['results']:
+            url = image_info['unescapedUrl']
+            if url != "":
+                url_list.append(url)
+
+    return url_list
+
+# Example usage
+# searchImageUrl('California', 10)
